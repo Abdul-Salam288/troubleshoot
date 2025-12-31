ec7d01f1fea9ec6083d1a3d952c787b77729c316
Abdul-Salam288 <58850561+Abdul-Salam288@users.noreply.github.com>
2026-01-01 01:35:19 +0530

    Update Access denied.sh

--- VM Access denied/Access denied.sh
+++ VM Access denied/Access denied.sh
@@ -1,1 +1,26 @@
 
+# Azure Linux login failure  or Permission denied or Access denied
+# SSH is working but authentication failure or no authentication
+
+# to verify the pv scan and vg scan 
+sudo pvscan
+sudo vgscan
+
+# To verify the mount and disk state
+lsblk -f
+cat /etc/fstab
+sudo fdisk -l /dev/sdc
+sudo parted /dev/sdc print
+
+# steps to recover
+sudo mkdir -p /mnt/rescue
+sudo mount /dev/mapper/vg0-root /mnt/rescue
+ls /mnt/rescue
+sudo chroot /mnt/rescue  	<-- this will behave like your on broken VM
+useradd -m -s /bin/bash azurefix
+passwd azurefix
+usermod -aG sudo azurefix
+exit
+sudo umount /mnt/rescue
+
+
